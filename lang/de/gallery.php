a4229dfe5551d311c7405c6cccf5f07504877bad
Guido Hornig <hornig@actxc.de>
2014-09-30 13:11:05 +0200

    added a German lang file de
    let me know, when you add this to the moodle database.

---
+++ lang/de/gallery.php
@@ -0,0 +1,94 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://elf.phil.muni.cz/moodledev/moodle2
+ * German translation from Guido Hornig, actXcellence
+ * @package    mod
+ * @subpackage gallery
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+defined('MOODLE_INTERNAL') || die;
+
+$string['addimages'] = 'Bilder hinzufügen';
+$string['attachments'] = 'Anhänge';
+$string['author'] = 'Autor';
+$string['batchrun'] = 'Start';
+$string['confirmdelete'] = 'Alle ausgewählten Bilder löschen?';
+$string['delete'] = 'Löschen';
+$string['deleteimage'] = 'Bild löschen';
+$string['description'] = 'Beschreibung:';
+$string['download'] = 'Herunterladen';
+$string['downloadphotos'] = 'Alle Bilder herunterladen';
+$string['downloadoriginalimage'] = 'Orginalbild anzeigen';
+$string['edit'] = 'Bearbeiten';
+$string['editimage'] = 'Bild bearbeiten';
+$string['editimages'] = 'Bilder bearbeiten';
+$string['gallery:addimages'] = 'Bild hinzufügen';
+$string['gallery:addinstance'] = 'Neue Galerie erzeugen';
+$string['gallery:deleteallimages'] = 'Alle Bilder löschen';
+$string['gallery:deleteownimages'] = 'Eigene Bilder löschen';
+$string['gallery:editallimages'] = 'Alle Bilder löschen';
+$string['gallery:editownimages'] = 'Eigene Bilder bearbeiten';
+$string['gallery:manage'] = 'Galerie Verwalten';
+$string['gallery:view'] = 'Galerie anzeigen';
+$string['galleryname'] = 'Name:'; 
+$string['image'] = 'Bild';
+$string['imageattachments'] = 'Anhang für jedes Bild erlauben';
+$string['imageattachments_help'] = 'If enabled, each image in your gallery can have files attach to it, which will only show with the particular image within the gallery preview mode. In this way, you can enhance the content of your gallery by texts, sounds, videos, and other files relevant for each image.';
+$string['imagename'] = 'Bildname';
+$string['images'] = 'Bilder';
+$string['missingsourceerror'] = 'Bitte die Quelle angeben.';
+$string['modulename'] = 'Galerie';
+$string['modulename_help'] = 'Bilder in einer Galerie im Kurs verwalten. 
+Es werden Mini-Bilder erzeugt, die als Navigation in der Galerie dienen. Die Minibilder können auch auf der Kursseite angezeigt werden. Jedes Bild muss mit einer Quellenangabe ausgestattet werden.
+<ul>
+<li>Trainer können Galerien anlegen. </li>
+<li>Teilnehmer können Bilder kommentieren.</li>
+<li>Bilder können auch mit Anhängen (Dokumente, Audio oder Video Dateien) ergänzt werden.</li>
+<li>Über Rechte kann Teilnehmern erlaubt werde Bilder hinzuzufügen, ohne Zugriff auf die ganze Galerie-Einstellungen zu bekommen.</li>
+'; 
+$string['modulenameplural'] = 'Galerien';
+$string['moveimage'] = 'Bild verschieben';
+$string['next'] = 'weiter';
+$string['nextimage'] = 'nächstes Bild';
+$string['nopermission'] = 'Sie haben keine Rechte um diese Aktion auszuführen.';
+$string['of'] = 'von';
+$string['pluginadministration'] = 'Galerie-Einstellungen';
+$string['pluginname'] = 'Galerie';
+$string['previews'] = 'Vorschau';
+$string['previous'] = 'zurück'; 
+$string['previousimage'] = 'vorheriges Bild';
+$string['returntogallery'] = 'Zurück zur Mini-Ansicht';
+$string['rotateleft'] = 'Links drehen';
+$string['rotateright'] = 'Rechts drehen';
+$string['savechanges'] = 'Änderungen speichern';
+$string['saveimages'] = 'Bilder speichern';
+$string['selectdeselectall'] = 'alle / keins';
+$string['selectedimageslabel'] = '<strong>mit ausgewählten:</strong>';
+$string['showoriginalimage'] = 'Link zum Orginal anzeigen';
+$string['showoriginalimage_help'] = 'If enabled, links to original image files will be displayed in the image presentation mode. Clicking on the link will bring up a new window diplaying a particular image in its original size and resolution.';
+$string['showthumbnails'] = 'Mini-Bilder auf der Kursseite anzeigen';
+$string['showthumbnails_help'] = 'If enabled, interactive thumbnails of all gallery images will be displayed directly on the course page.';
+$string['source'] = 'Quelle';
+$string['sourceown'] = 'Mein eigenes Bild';
+$string['thumbnails'] = 'Mini-Bilder';
+$string['turneditingoff'] = 'Bearbeiten ausschalten';
+$string['turneditingon'] = 'Bearbeiten einschaltenn';
+$string['uploadimages'] = 'Bilder hochladen';
+$string['viewpreview'] = 'Galerie ansehen';
